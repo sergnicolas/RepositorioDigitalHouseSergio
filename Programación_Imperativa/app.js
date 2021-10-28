9b0a6baf2c9948541cccefc1a833b3a9ba64f1a6
sergnicolas <sergionicolasguerrero@gmail.com>
2021-10-28 09:36:24 -0300

    repositorio de DH

---
+++ Programación_Imperativa/app.js
@@ -0,0 +1,16 @@
+let cajonRemeras = "pantalones"
+let cajonPantalones = "remeras"
+console.log("antes de los cambios");
+console.log("cajón de remeras: " + cajonRemeras);
+console.log("cajón de pantalones: " + cajonPantalones);
+
+
+let enLaCama = cajonRemeras
+cajonRemeras = cajonPantalones
+cajonPantalones = enLaCama
+console.log("despues de los cambios");
+console.log("cajón de remeras: " + cajonRemeras);
+console.log("cajón de pantalones: " + cajonPantalones);
+
+
+
---
+++ Programación_Imperativa/app4.js
@@ -0,0 +1,25 @@
+function chorus(animal, sonido){
+    const texto = "old mc donal have a farm iao " + 
+    animal + 
+    " que hace "+ 
+    sonido + 
+    ", " + 
+    sonido + 
+    ", " + 
+    sonido + 
+    ", " + 
+    sonido;
+    return texto
+}
+
+/* console.log(chorus("cerdo", "oink"));
+console.log(chorus("vaca", "muu")); */
+
+function f(x){
+    return 2 * x + 5;
+}
+const pepe = 20;
+
+console.log( f(2));
+console.log( f(5));
+console.log(f(pepe));
